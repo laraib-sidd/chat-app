49f27bac305ea60f1800cc3a5c3e332dc652290f
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-14 19:42:18 +0530

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,6 +2,11 @@ const socket = io();
 const h1 = document.querySelector("h1");
 const inp = document.querySelector("input");
 const bt = document.querySelector("button");
+
+bt.addEventListener('click',()=>{
+    console.log(inp.value);
+})
+
 socket.on("message", (mes) => {
 	h1.innerText = mes;
 });
