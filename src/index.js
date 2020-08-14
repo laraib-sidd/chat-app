122e4757d0916d8c4975fec6f0c4ecc3daf9fbd8
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-14 18:07:14 +0530

    updae

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+const path = require("path");
 const express = require("express");
+
 const app = express();
 
 const port = process.env.PORT;
-const path = __dirname + "/public";
+const publiDirectoryPath = path.join(__dirname,"../public");
 
-console.log(port);
+app.use(express.static(publiDirectoryPath));
+
+app.listen(port, () => {
+	console.log("The server is running on port ", port);
+});
