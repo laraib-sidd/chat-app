336e533c2b98e0583a8e2d6ecb71454e5b637f84
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-14 19:42:45 +0530

    udpate

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,9 +3,9 @@ const h1 = document.querySelector("h1");
 const inp = document.querySelector("input");
 const bt = document.querySelector("button");
 
-bt.addEventListener('click',()=>{
-    console.log(inp.value);
-})
+bt.addEventListener("click", () => {
+	console.log(inp.value);
+});
 
 socket.on("message", (mes) => {
 	h1.innerText = mes;
