2f109f9af5384aedbf82085214fe684749c2183f
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-14 21:08:31 +0530

    udpate

--- src/index.js
+++ src/index.js
@@ -19,6 +19,9 @@ io.on("connection", (socket) => {
 	socket.on("sendMessage", (message) => {
 		io.emit("message", message);
 	});
+	socket.on("disconnect", () => {
+		io.emit("message", "A user has left");
+	});
 });
 
 server.listen(port, () => {
