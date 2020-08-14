ee875b4406c6a8581cbd2472fcae95bd56b5076c
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-14 18:19:49 +0530

    udpate

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const path = require("path");
 const express = require("express");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
 
 const port = process.env.PORT;
-const publiDirectoryPath = path.join(__dirname,"../public");
+const publiDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publiDirectoryPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log("The server is running on port ", port);
 });
