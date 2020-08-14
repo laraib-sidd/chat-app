4a31f19faaea4f674173871aadca8dd86aa95946
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-14 19:08:31 +0530

    update

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ io.on("connection", (socket) => {
 	socket.emit("countUpdated", count);
 	socket.on("increment", () => {
 		count++;
-		socket.emit('countUpdated',count)
+		io.emit("countUpdated", count);
 	});
 });
 
