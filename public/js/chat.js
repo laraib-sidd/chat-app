a8a4b79eb4264ba270768aeb9fd1d46acee6b614
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-14 18:52:36 +0530

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,6 +2,7 @@ const socket = io();
 socket.on("countUpdated", (count) => {
 	console.log("The count has been updated to ", count);
 });
+
 const bt = document.querySelector("button");
 bt.addEventListener("click", (e) => {
 	count += 1;
