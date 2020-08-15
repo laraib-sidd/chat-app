a7dfa43c931d2a9dfb5523b6fe099ebecc498591
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-15 20:10:26 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -16,9 +17,14 @@ io.on("connection", (socket) => {
 	socket.emit("message", "Welcome!");
 	socket.broadcast.emit("message", "A new user has entered");
 
-	socket.on("sendMessage", (message,callback) => {
-		io.emit("message", message);
-		callback()
+	socket.on("sendMessage", (message, callback) => {
+		const filter = new Filter();
+		if (filter.isProfane(message)) {
+			return callback("Profanity is not allowed");
+		} else {
+			io.emit("message", message);
+		}
+		callback();
 	});
 	socket.on("disconnect", () => {
 		io.emit("message", "A user has left");
