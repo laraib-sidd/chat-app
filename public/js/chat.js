eb357090f901cc90ec1f8edee6a9ba63b5ae5b6a
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-15 21:46:17 +0530

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,7 +13,7 @@ const messageTemplate = document.querySelector("#message-template").innerHTML;
 socket.on("message", (message) => {
 	console.log(message);
 	const html = Mustache.render(messageTemplate);
-	$messages.insertAdjacentHTML('beforeend	',html);
+	$messages.insertAdjacentHTML('beforeend',html);
 });
 
 $messageForm.addEventListener("submit", (e) => {
