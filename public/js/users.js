5b5b1ceee48ae31941038f20282b93f913721753
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-18 17:17:22 +0530

    udpate

---
+++ public/js/users.js
@@ -0,0 +1,12 @@
+const users = [];
+const addUser = () => {};
+const removeUser = () => {};
+const getUser = () => {};
+const getUsersInRoom = () => {};
+
+module.exports = {
+	addUser,
+	removeUser,
+	getUser,
+	getUsersInRoom,
+};
