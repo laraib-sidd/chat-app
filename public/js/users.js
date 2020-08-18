70090e8f565e13dcd10a09add722113607756a02
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-18 17:19:53 +0530

    upate

--- public/js/users.js
+++ public/js/users.js
@@ -1,5 +1,16 @@
 const users = [];
-const addUser = () => {};
+const addUser = ({ id, username, room }) => {
+	// Clean the data
+	username = username.trim().toLowerCase();
+	room = room.trim().toLowerCase();
+
+	// Validate the statement
+	if (!username || !room) {
+		return {
+			error: "Username and room are required",
+		};
+	}
+};
 const removeUser = () => {};
 const getUser = () => {};
 const getUsersInRoom = () => {};
