4505a440d9b8230d0991fe10f339a13f5f9c1bb3
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-18 19:12:23 +0530

    remoe

--- public/js/users.js
+++ public/js/users.js
@@ -28,7 +28,12 @@ const addUser = ({ id, username, room }) => {
 	users.push(user);
 	return { user };
 };
-const removeUser = () => {};
+const removeUser = (id) => {
+	const index = users.findIndex((user) => user.id === id);
+	if (index !== -1) {
+		return users.splice(index, 1)[0];
+	}
+};
 const getUser = () => {};
 const getUsersInRoom = () => {};
 
