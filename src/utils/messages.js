03e2990e96532f7f3aa62f414fa76947ee2738e4
laraib-sidd <laraib.sidd7@gmail.com>
2020-08-18 23:08:43 +0530

    sa

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,8 +1,8 @@
 const generateMessage = (text) => {
 	return { text, createdAt: new Date().getTime() };
 };
-const generateLocationMessage = (url) => {
-	return { url, createdAt: new Date().getTime() };
+const generateLocationMessage = (username, url) => {
+	return { username, url, createdAt: new Date().getTime() };
 };
 
 module.exports = {
